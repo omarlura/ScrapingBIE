deb880ad7ff65b49de88057fbc66d2b86408ccc9
torocar <omarlura@gmail.com>
2025-06-26 16:08:00 -0500

    Modificación config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
-EMAIL_USER      = "omar.ura@bisionconsulting.com"
-EMAIL_PASSWORD  = "wvmclzvrdtgyqphs"
-EMAIL_RECEIVER  = "omar.ura@bisionconsulting.com"
-TELEGRAM_TOKEN  = "8059564784:AAGQzHdqihODBJRfcAd5RC0Z1cJOnqOqif4"
-TELEGRAM_CHAT_ID = "8128681184"
-TOKEN_API_EVENTBRITE = "6SIQKLK2OOQ23NXKKT5X"
+EMAIL_USER      = ""
+EMAIL_PASSWORD  = ""
+EMAIL_RECEIVER  = ""
+TELEGRAM_TOKEN  = ""
+TELEGRAM_CHAT_ID = ""
+TOKEN_API_EVENTBRITE = ""
